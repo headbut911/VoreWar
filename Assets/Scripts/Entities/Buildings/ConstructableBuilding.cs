2557f4a2b0e7af519d8140ac5ec51b149757de61
CaneSugarCat <137650128+CaneSugarCat@users.noreply.github.com>
2024-08-25 09:54:35 -0700

    Begin setup

---
+++ Assets/Scripts/Entities/Buildings/ConstructableBuilding.cs
@@ -0,0 +1,23 @@
+﻿using OdinSerializer;
+
+
+abstract class ConstructableBuilding
+{
+    [OdinSerialize]
+    internal Empire Owner;
+
+    [OdinSerialize]
+    internal Vec2i Position;
+
+    [OdinSerialize]
+    internal int UpgradeStage;
+
+    protected ConstructableBuilding(Vec2i location)
+    {
+        Position = location;
+    }
+
+    internal abstract void RunBuildingFunction();
+
+}
+
